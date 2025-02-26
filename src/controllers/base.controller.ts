654244a2639754ddfa6d9ebee170e72215115607
jesusesteban2 <jesus.esteban@outlook.com>
2025-02-26 19:14:09 +0100

    Reorganice modules

--- src/controllers/base.controller.ts
+++ src/controllers/base.controller.ts
@@ -1,29 +1,29 @@
 import type { NextFunction, Request, Response } from 'express';
 import createDebug from 'debug';
-import { HttpError } from '../errors/http-error.js';
+import { HttpError } from './errors.controller';
 
 export const notFoundController = (
-    req: Request,
-    _res: Response,
-    next: NextFunction,
+  req: Request,
+  _res: Response,
+  next: NextFunction,
 ) => {
-    const debug = createDebug('demo:notFoundController');
-    debug('Petición recibida');
+  const debug = createDebug('App:notFoundController');
+  debug('Petición recibida');
 
-    const message = `Page ${req.url} not found`;
-    const error = new HttpError(message, 404, 'Not Found');
-    next(error);
+  const message = `Page ${req.url} not found`;
+  const error = new HttpError(message, 404, 'Not Found');
+  next(error);
 };
 
 export const notMethodController = (
-    req: Request,
-    _res: Response,
-    next: NextFunction,
+  req: Request,
+  _res: Response,
+  next: NextFunction,
 ) => {
-    const debug = createDebug('demo:notMethodController');
-    debug('Petición recibida');
+  const debug = createDebug('demo:notMethodController');
+  debug('Petición recibida');
 
-    const message = `Method ${req.method}  not allowed`;
-    const error = new HttpError(message, 405, 'Method Not Allowed');
-    next(error);
+  const message = `Method ${req.method}  not allowed`;
+  const error = new HttpError(message, 405, 'Method Not Allowed');
+  next(error);
 };
--- src/controllers/errors.controller.ts
+++ src/controllers/errors.controller.ts
@@ -1,30 +1,40 @@
 import { Request, Response, NextFunction } from 'express';
 import createDebug from 'debug';
-import { HttpError } from '../errors/http-error.js';
 // import { ErrorPage } from '../views/pages/error-page.js';
 
 const debug = createDebug('demo:errorManager');
 
 export const errorManager = (
-    err: HttpError | Error,
-    _req: Request,
-    res: Response,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    _next: NextFunction,
+  err: HttpError | Error,
+  _req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _next: NextFunction,
 ) => {
-    if (!('status' in err)) {
-        err = {
-            ...err,
-            statusCode: 500,
-            status: 'Internal Server Error',
-        };
-    }
+  if (!('status' in err)) {
+    err = {
+      ...err,
+      statusCode: 500,
+      status: 'Internal Server Error',
+    };
+  }
 
-    const publicMessage = `Error: ${err.statusCode} ${err.status}`;
-    // const view = new ErrorPage();
-    debug(publicMessage, err.message);
+  const publicMessage = `Error: ${err.statusCode} ${err.status}`;
+  // const view = new ErrorPage();
+  debug(publicMessage, err.message);
 
-    res.status(err.statusCode);
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    // res.send(view.render({ errorMessage: publicMessage }));
+  res.status(err.statusCode);
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  // res.send(view.render({ errorMessage: publicMessage }));
 };
+
+export class HttpError extends Error {
+  constructor(
+    message: string,
+    public statusCode: number,
+    public status: string,
+  ) {
+    super(message);
+    this.name = 'HttpError';
+  }
+}
