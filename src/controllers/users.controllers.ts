6d287f28beda676a5adce2fe9fe06f533f60f163
jesusesteban2 <jesus.esteban@outlook.com>
2025-03-05 21:22:02 +0100

    Update user controller

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -3,15 +3,20 @@ import { User } from '@prisma/client';
 import { UserRepo } from '../repo/repositorytype.js';
 import { AppResponse } from '../middleware/responseJson.js';
 import { UserCreateDTO, UserLoginDTO } from '../DTO/users.dto.js';
+import { AuthService } from '../services/auth.service.js';
 
 export class UsersController {
     constructor(private repoUser: UserRepo<User>) {}
 
     async register(req: Request, res: Response, next: NextFunction) {
         try {
-            const { body } = req.body;
+            const body = req.body;
+            console.log(body);
             UserCreateDTO.parse(body);
+            body.password = await AuthService.hashPassword(body.password);
             const user = await this.repoUser.register(body);
+            user.password = '';
+            user.id = '';
             const data: AppResponse<User> = {
                 data: [user],
                 error: '',
@@ -24,9 +29,12 @@ export class UsersController {
 
     async login(req: Request, res: Response, next: NextFunction) {
         try {
-            const { body } = req.body;
+            const body = req.body;
+            console.log(body);
             UserLoginDTO.parse(body);
             const user = await this.repoUser.login(body.email, body.password);
+            user.password = '';
+            user.id = '';
             const data: AppResponse<User> = {
                 data: [user],
                 error: '',
--- src/middleware/responseJson.ts
+++ src/middleware/responseJson.ts
@@ -10,3 +10,5 @@ export class AppResponse<T> {
         public error: string,
     ) {}
 }
+
+//public data: Omit<T, 'password'>[],
